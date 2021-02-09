a790598c4406ed84dbbcfa1011a4de05eb1a8f22
Nilton Pontes <nilton.pontes@jbq.global>
2021-02-08 22:22:11 -0300

    feat: create alert message

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { StyleSheet, StatusBar, Text, View } from 'react-native';
+import { StyleSheet, StatusBar, Text, View, Alert } from 'react-native';
 import { WebView } from 'react-native-webview';
+import ErrorScreen from './components/ErrorScreen';
 
 export default function App() {
   return (
-    <>
-      <StatusBar></StatusBar>
-      <WebView source={{ uri: 'https://littlecoffee.vercel.app'}} />
-    </>
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor="#252422" translucent />
+      <WebView source={{ uri: 'https://littlecoffee.vercel.app'}} onError={() => {
+        Alert.alert('Oops...', 'Não foi possível carregar o app.')
+      }} style={styles.webViewContainer}/>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#252422',
+    flex: 1,
+  },
+  webViewContainer: {
+    flex: 1,
+    marginTop: 24,
+  }
+})
